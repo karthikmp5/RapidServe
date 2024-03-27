bcc59ae5f08e3889262a8fdfc53a83491816a662
karthikmp5 <kmasine@ncsu.edu>
2024-03-27 22:08:04 +0000

    southbound creating ovs_bridges

--- southbound/retrive_etcd.py
+++ southbound/retrive_etcd.py
@@ -3,6 +3,20 @@ import etcd3
 # Create an instance of Etcd3Client
 client = etcd3.client()
 
+# def retrieve_all_data_from_etcd():
+#     all_data = {}
+
+#     db = client.get_all()
+
+#     for value, addr in db:
+#         value_str = value.decode()
+#         print(value_str)
+
+#         # key = 
+
+#     # return all_data
+
+
 def retrieve_data_from_etcd(key):
     # Retrieve data associated with the given key
     value, _ = client.get(key)
@@ -10,7 +24,7 @@ def retrieve_data_from_etcd(key):
 
 if __name__ == "__main__":
     # Example key to retrieve data
-    example_key = "hotstar" #One/subnet11/n111"
+    example_key = "netflix" #One/subnet11/n111"
 
     # Retrieve data from etcd
     data = retrieve_data_from_etcd(example_key)
@@ -20,3 +34,14 @@ if __name__ == "__main__":
         print(data)
     else:
         print("Data not found for the given key.")
+
+
+
+    # all_data = retrieve_all_data_from_etcd()
+
+    # if all_data:
+    #     print("Retrieved data:")
+    #     for key, value in all_data.items():
+    #         print(f"Key: {key}, Value: {value}")
+    # else:
+    #     print("No data found in etcd.")

e5d97922fa9ff665caf3a425282509b0c2ab4a2b
karthikmp5 <kmasine@ncsu.edu>
2024-03-31 20:41:18 +0000

    adding state controller

--- southbound/retrive_etcd.py
+++ southbound/retrive_etcd.py
@@ -1,4 +1,6 @@
 import etcd3
+import sys
+import ipaddress
 
 # Create an instance of Etcd3Client
 client = etcd3.client()
@@ -24,16 +26,67 @@ def retrieve_data_from_etcd(key):
 
 if __name__ == "__main__":
     # Example key to retrieve data
-    example_key = "netflix" #One/subnet11/n111"
+    example_key = sys.argv[1] #"netflix" #One/subnet11/n111"
 
     # Retrieve data from etcd
-    data = retrieve_data_from_etcd(example_key)
+    data_str = retrieve_data_from_etcd(example_key)
 
-    if data:
-        print("Retrieved data:")
+    if data_str:
+        # Convert retrieved data string to dictionary
+        data = eval(data_str)
+        # print("Retrieved data: ", data)
+
+        # Iterate through each VPC
+        for vpc in data[example_key]['vpcs']:
+            # Iterate through each subnet in the VPC
+            for subnet in vpc['subnets']:
+                cidr = subnet['CIDR']
+                
+                # Extract the network address and prefix length from the CIDR
+                network = ipaddress.ip_network(cidr)
+                ip = str(network.network_address + 1)  # IP is the first address in the CIDR block
+                dhcp_start = str(network.network_address + 2)  # DHCP start is the second address
+                dhcp_end = str(network.broadcast_address - 1)  # DHCP end is the last but one address
+
+                # Add the new fields to the subnet
+                subnet['ip'] = ip
+                subnet['dhcp_start'] = dhcp_start
+                subnet['dhcp_end'] = dhcp_end
+
+
+                # # Extract the first three octets from the CIDR
+                # first_three_octets = '.'.join(cidr.split('.')[:3])
+
+                # # Construct IP, DHCP start, and DHCP end using the first three octets
+                # ip = f"{first_three_octets}.1"
+                # dhcp_start = f"{first_three_octets}.2"
+                # dhcp_end = f"{first_three_octets}.254"
+
+                # # Add the new fields to the subnet
+                # subnet['ip'] = ip
+                # subnet['dhcp_start'] = dhcp_start
+                # subnet['dhcp_end'] = dhcp_end
+
+                # Print the updated subnet information
+                # print(f"CIDR: {cidr}")
+                # print(f"  IP: {ip}")
+                # print(f"  DHCP Start: {dhcp_start}")
+                # print(f"  DHCP End: {dhcp_end}")
+                # print()
+        print("Updated data:")
         print(data)
     else:
         print("Data not found for the given key.")
+
+
+    # # Retrieve data from etcd
+    # data = retrieve_data_from_etcd(example_key)
+
+    # if data:
+    #     print("Retrieved data:")
+    #     print(data)
+    # else:
+    #     print("Data not found for the given key.")
 
 
 

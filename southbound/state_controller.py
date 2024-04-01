6ba49f1128aaeb6ae2d78569834d13773d6414d8
karthikmp5 <kmasine@ncsu.edu>
2024-04-01 06:04:09 +0000

    update state controller

--- southbound/state_controller.py
+++ southbound/state_controller.py
@@ -14,39 +14,54 @@ def run_playbook(filename, vars_dict):
         'extravars': vars_dict
     }
     result = run(**config)
-    return
+    return result
 
 def create_infra_resources(tenant_name, tenant_data):
-    run_playbook('deploy_infra.yaml', {'tenant_name': [tenant_name]})
-    return
+    result = run_playbook('deploy_infra.yaml', {'tenant_name': [tenant_name]})
+    return result
 
 def create_VM_resources(tenant_name, tenant_data):
-    run_playbook('deploy_VMs.yaml', {'tenant_name': [tenant_name]})
-    return
+    result = run_playbook('deploy_VMs.yaml', {'tenant_name': [tenant_name]})
+    return result
 
 def create_cdn_resources(tenant_name, tenant_data):
-    run_playbook('deploy_cdn.yaml', {'tenant_name': [tenant_name]})
-    return
+    result = run_playbook('deploy_cdn.yaml', {'tenant_name': [tenant_name]})
+    return result
+
+def check_and_update_tenant_state(tenant_name, tenant_data):
+    if  tenant_data[tenant_name]['state'] == 'requested':
+        result = create_infra_resources(tenant_name, tenant_data)
+        
+        if result.rc == 0:
+            tenant_data[tenant_name]['state'] = 'active'
+            client.put(tenant_name, str(tenant_data))
+        else:
+            print("Error occurred while creating infrastructure resources.")
+            sys.exit(1)
 
 def check_and_update_VM_state(tenant_name, tenant_data):
     for vpc in tenant_data[tenant_name]['vpcs']:
         if vpc['VMs_state'] == 'requested':                             
-            create_VM_resources(tenant_name, tenant_data)
-            vpc['VMs_state'] = 'active'
-        break
-    client.put(tenant_name, str(tenant_data))
+            result = create_VM_resources(tenant_name, tenant_data)
 
-def check_and_update_tenant_state(tenant_name, tenant_data):
-    if  tenant_data[tenant_name]['state'] == 'requested':
-        create_infra_resources(tenant_name, tenant_data)
-        tenant_data[tenant_name]['state'] = 'active'
-    client.put(tenant_name, str(tenant_data))
+            if result.rc == 0:
+                vpc['VMs_state'] = 'active'
+                client.put(tenant_name, str(tenant_data))
+                break
+            else:
+                print("Error occurred while creating VM resources.")
+                sys.exit(1)
 
 def check_and_update_cdn(tenant_name, tenant_data):
     if  tenant_data[tenant_name]['cdn']['state'] == 'requested':
-        create_cdn_resources(tenant_name, tenant_data)
-        tenant_data[tenant_name]['cdn']['state'] = 'active'
-    client.put(tenant_name, str(tenant_data))
+        result = create_cdn_resources(tenant_name, tenant_data)
+        
+        if result.rc == 0:
+            tenant_data[tenant_name]['cdn']['state'] = 'active'
+            client.put(tenant_name, str(tenant_data))
+        else:
+            print("Error occurred while creating cdn resources.")
+            sys.exit(1)
 
 if __name__ == "__main__":
 
@@ -58,8 +73,8 @@ if __name__ == "__main__":
     if value:
         tenant_data = eval(value.decode())  
         print("tenant_data = ", tenant_data)
-        # check_and_update_tenant_state(example_key, tenant_data)
-        # check_and_update_VM_state(example_key, tenant_data)
+        check_and_update_tenant_state(example_key, tenant_data)
+        check_and_update_VM_state(example_key, tenant_data)
         check_and_update_cdn(example_key, tenant_data)
     else:
         print(f"No data found for key: {example_key}")

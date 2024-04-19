bf351d6a285c97078ca00ab7c04be46ac904afa2
karthikmp5 <kmasine@ncsu.edu>
2024-04-19 22:42:03 +0000

    modularized code IaaS

--- southbound/retrive_etcd.py
+++ southbound/retrive_etcd.py
@@ -20,26 +20,26 @@ if __name__ == "__main__":
     if data_str:
         # Convert retrieved data string to dictionary
         data = eval(data_str)
-        # print("Retrieved data: ", data)
+        # # print("Retrieved data: ", data)
 
-        # Iterate through each VPC
-        for vpc in data[tenant_name]['vpcs']:
-            # Iterate through each subnet in the VPC
-            for subnet in vpc['subnets']:
-                cidr = subnet['CIDR']
+        # # Iterate through each VPC
+        # for vpc in data[tenant_name]['vpcs']:
+        #     # Iterate through each subnet in the VPC
+        #     for subnet in vpc['subnets']:
+        #         cidr = subnet['CIDR']
                 
-                # Extract the network address and prefix length from the CIDR
-                network = ipaddress.ip_network(cidr)
-                ip = str(network.network_address + 1)  # IP is the first address in the CIDR block
-                dhcp_start = str(network.network_address + 2)  # DHCP start is the second address
-                dhcp_end = str(network.broadcast_address - 1)  # DHCP end is the last but one address
+        #         # Extract the network address and prefix length from the CIDR
+        #         network = ipaddress.ip_network(cidr)
+        #         ip = str(network.network_address + 1)  # IP is the first address in the CIDR block
+        #         dhcp_start = str(network.network_address + 2)  # DHCP start is the second address
+        #         dhcp_end = str(network.broadcast_address - 1)  # DHCP end is the last but one address
 
-                # Add the new fields to the subnet
-                subnet['ip'] = ip
-                subnet['dhcp_start'] = dhcp_start
-                subnet['dhcp_end'] = dhcp_end
+        #         # Add the new fields to the subnet
+        #         subnet['ip'] = ip
+        #         subnet['dhcp_start'] = dhcp_start
+        #         subnet['dhcp_end'] = dhcp_end
 
-        # print("%s data is" % tenant_name)
+        # # print("%s data is" % tenant_name)
         print("Updated data:")
         print(data)
     else:
